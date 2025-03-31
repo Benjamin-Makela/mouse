388fa9b7b1b4263d82a8c487063a1ed12fa3db94
makelben-LAHS <100030354@mvla.net>
2025-03-31 13:28:59 -0700

    This might work

--- mouserver.js
+++ mouserver.js
@@ -1,10 +1,13 @@
 const http = require("http");
+const fs = require("fs");
 const port = 80;
 
 const server = http.createServer((req, res) => {
     if (req.url === "/") {
-        res.writeHead(200, { "Content-Type": "text/html" });
-        res.end("<p>So this works I guess.</p>");
+        fs.readFile(path.join(__dirname, "mouse.html"), (data, err) => {
+            res.writeHead(200, { "Content-Type": "text/html" });
+            res.end();
+        });
     } else {
         res.writeHead(404, { "Content-Type": "text/html" });
         res.end("Not working yo. There is no page with this address.");
