51f4db715cd9b4307d9dc4ce22fdc09e076cb959
makelben-LAHS <100030354@mvla.net>
2025-03-31 13:33:02 -0700

    adding debug statements

--- mouserver.js
+++ mouserver.js
@@ -5,7 +5,8 @@ const port = 80;
 
 const server = http.createServer((req, res) => {
     if (req.url === "/") {
-        fs.readFile(path.join(__dirname, "mouse.html"), (data, err) => {
+        const dir = path.join(__dirname, "mouse.html");
+        fs.readFile(dir, (data, err) => {
             res.writeHead(200, { "Content-Type": "text/html" });
             res.end();
         });
