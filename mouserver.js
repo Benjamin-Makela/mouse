f8154e7746b4cd0aeb7dd0c2d37ba0b9abb9a3a3
makelben-LAHS <100030354@mvla.net>
2025-03-31 09:44:17 -0700

    Now it will work

--- mouserver.js
+++ mouserver.js
@@ -1,7 +1,7 @@
 const http = require("http");
 const port = 80;
 
-http.createServer((req, res) => {
+const server = http.createServer((req, res) => {
     if (req.url === "/") {
         res.writeHead({ "Content-Type": "text/html" });
         res.end(200, "<p>So this works I guess.</p>");
